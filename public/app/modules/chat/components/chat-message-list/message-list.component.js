eb115a542de851087c0b95e4b0f67086def98c9a
akshaykalola.rapidops <akshay.kalola@rapidops.com>
2017-12-22 04:10:09 +0530

    directive commmit

--- public/app/modules/chat/components/chat-message-list/message-list.component.js
+++ public/app/modules/chat/components/chat-message-list/message-list.component.js
@@ -41,17 +41,6 @@
          * @ticket BOMB-1491, BOMB-1933
          */
         function activate() {
-            ChatService.getMessages().then(function(data){
-                _.forEach(data.data, function(data){
-                    if(data.msgFrom == $location.search().id){
-                        data.isLoggingUser = true;
-                    } else {
-                        data.isLoggingUser = false;
-                    }
-                    vm.messages.push(data);
-                });
-                console.log(vm.messages);
-            })
             $rootScope.$on('SEND_MESSAGE',function(event,data){
                 if(data.data){
                     if(data.data.msgFrom == $location.search().id){
@@ -79,7 +68,18 @@
             $rootScope.$on('userChange',function(event, data){
                 vm.messages = [];
                 //$scope.$apply();
+                ChatService.getMessages().then(function(data){
+                    _.forEach(data.data, function(data){
+                        if(data.msgFrom == $location.search().id){
+                            data.isLoggingUser = true;
+                        } else {
+                            data.isLoggingUser = false;
+                        }
+                        vm.messages.push(data);
+                    });
+                });
             });
+
         }
     }
 })();
