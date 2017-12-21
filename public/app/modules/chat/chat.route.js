7025a104f2a13ebe6a74d2d7484e8c8d8b260015
rushipandya <rushi.pandya@rapidops.com>
2017-12-21 19:54:05 +0530

    chat modules design

--- public/app/modules/chat/chat.route.js
+++ public/app/modules/chat/chat.route.js
@@ -10,5 +10,9 @@
 				url:'/chat',
 				template:'<div>dinesh </div><chat-main>hello there</chat-main>'
 			})
+            .state('app.chatTemp', {
+                url:'/chat-temp',
+                templateUrl:'/app/modules/chat/template/chat-temp.html'
+            })
 	}
 })();
